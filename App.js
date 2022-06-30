d620cc9c1a2e5394eea9ae401e2eabd1ddc0fce4
Jashwant1997 <jashwantmaharaj420@gmail.com>
2022-06-30 15:57:25 +0500

    added textinput and button

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello! My name is Jashwant</Text>
+    <View style={styles.ContainerStyle}>
+      <View>
+        <TextInput placeholder='Course Goals...' style={styles.TextInputStyle} />
+        <Button title='ADD' style={styles.ButtonStyle}/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  ContainerStyle:{
+    padding:50
   },
+  TextInputStyle:{
+    padding:10,
+    fontSize:16,
+    borderBottomColor:'black',
+    borderBottomWidth:2,
+    marginBottom:15
+  },
+  ButtonStyle:{
+    
+  }
 });
+
+export default App;
